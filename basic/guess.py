ae5a65742d773a9f9f6cd73cf074b44d23dbbfb9
Preachyauto829 <ethanpoontsui@gmail.com>
2021-03-12 22:41:36 +0000

    add more files

---
+++ basic/guess.py
@@ -0,0 +1,12 @@
+Number = input("What is the number??")
+print("Number is: {0}".format(Number))
+
+
+#edit below for game
+
+a = 'Hello Somebody'
+print("Repeating \"{0}\" {1} times: {2}".format(a, 4, a * 4))
+#print(a * 50)
+
+print("Try the named index placegolder:")
+print("Name: {name}, Age: {age}".format(age=13, name="Ethan"))
